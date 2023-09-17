8b2965e8f71fdd03454c0680d3968f3053312f5f
aniruddhdadhich <aniruddhddh17@gmail.com>
2023-09-17 17:11:10 +0530

    initial commit

---
+++ src/com/aniruddh/oopBasics/Main.java
@@ -0,0 +1,13 @@
+package com.aniruddh.oopBasics;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // store 5 roll no.(s)
+        int[] nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        System.out.println(Arrays.toString(nums));
+    }
+}
